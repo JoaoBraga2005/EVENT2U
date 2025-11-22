f229fb064a9a838cfb5d8576c7ae0d2fc465fed1
Louise Mayumi <louise.mayumi@ufms.br>
2025-11-22 15:27:20 -0400

    Delete models directory

--- models/base.py
+++
@@ -1,5 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, Integer, String, DateTime, Date, Float, Text, ForeignKey, PrimaryKey 
-from sqlalchemy.orm import declarative_base
-
-Base = declarative_base()
